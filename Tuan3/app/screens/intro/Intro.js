785ffaa1a9b2d926712ea40b04a03a9abf63ac4d
NguyenDink <145578651+NguyenDink@users.noreply.github.com>
2024-09-12 00:48:16 +0700

    add: Intro.js

---
+++ Tuan3/app/screens/intro/Intro.js
@@ -0,0 +1,56 @@
+import React, { useEffect } from "react";
+import { StyleSheet, Text, View, Image } from "react-native";
+import { getToken } from "../../utils/AuthStorage";
+import { introspect } from "../../services/AuthAPIService";
+
+// Import hình ảnh từ thư mục cục bộ
+import logo from "../../assets/logo.jpg";
+
+export default function Intro({ navigation }) {
+    useEffect(() => {
+        const checkToken = async () => {
+            const token = await getToken();
+            if (token) {
+                const data = await introspect(token);
+
+                if (data.success) {
+                    navigation.replace("Home");
+                    return;
+                }
+            }
+            navigation.replace("Start");
+        };
+
+        const timer = setTimeout(() => {
+            checkToken();
+        }, 2000);
+
+        return () => clearTimeout(timer);
+    }, [navigation]);
+
+    return (
+        <View style={styles.container}>
+            <Image source={logo} style={styles.image} />
+            <Text style={styles.textName}>qnspJob</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+        padding: 20,
+    },
+    image: {
+        width: 120,
+        height: 120,
+        borderRadius: 10,
+        marginBottom: 20,
+    },
+    textName: {
+        fontSize: 24,
+    },
+});
